9dd1dabe6815174c1347dfd5ad47b1242f066365
kkeefe <kevinpk@hawaii.edu>
2020-12-04 14:57:51 -0800

    removing unused bloat packages

--- modules/emacs/config.el
+++ modules/emacs/config.el
@@ -18,8 +18,8 @@
 ;;
 ;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
 ;; font string. You generally only need these two:
-(setq doom-font (font-spec :family "monospace" :size 16 :weight 'semi-light)
-      doom-variable-pitch-font (font-spec :family "sans" :size 13))
+(setq doom-font (font-spec :family "Monaco" :size 16 ))
+      ;; doom-variable-pitch-font (font-spec :family "Courier" :size 13))
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
@@ -28,7 +28,6 @@
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
-(setq org-directory "~/org/")
 ;; local setqs
 (setq default-directory "~/")
 
@@ -83,6 +82,8 @@
 
 ;; for configuring more org things we want
 (after! org
+  (setq org-directory "~/org/")
+  (setq org-list-allow-alphabetical t)
   (setq org-agenda-files
         '("~/org/" "~/play/README.org"))
   (advice-add 'org-refile :after 'org-save-all-org-buffers))
@@ -99,9 +100,6 @@
 (setq evil-split-window-below t
       evil-vsplit-window-right t)
 
-;; just support this stuff bru
-(setq org-list-allow-alphabetical t)
-
 ;; additional personal things:
 ;; ;; org keybinds to set
 (global-set-key (kbd "C-c a") 'org-agenda)
@@ -111,46 +109,46 @@
 ;; list of custom org templates
 (after! org
   (setq org-capture-templates
-        '(("w" "work" entry (file+headline "~/Documents/org_files/work_done.org" "Work_Tasks")
+        '(("w" "work" entry (file+headline "~/org/work_done.org" "Work_Tasks")
           "* TODO %?\n:Description:\n %^t \n %i \n")
-          ("s" "self-stuff" entry (file+headline "~/Documents/org_files/work_done.org" "Self_Tasks")
+          ("s" "self-stuff" entry (file+headline "~/org/work_done.org" "Self_Tasks")
           "* TODO %?\n:Description:\n %^t \n %i \n")
-          ("v" "quick work" entry (file+headline "~/Documents/org_files/work_done.org" "Quick_Tasks")
+          ("v" "quick work" entry (file+headline "~/org/work_done.org" "Quick_Tasks")
           "* TODO %?\n:Description:\n %^t \n %i \n")
-          ("c" "creative" entry (file+headline "~/Documents/org_files/work_done.org" "Creative_Work")
+          ("c" "creative" entry (file+headline "~/org/work_done.org" "Creative_Work")
           "* TODO %?\n:Description:\n%^t \n %i \n")
-          ("p" "code problems" entry (file+headline "~/Documents/org_files/current_bugs.org" "Current Bugs")
+          ("p" "code problems" entry (file+headline "~/org/current_bugs.org" "Current Bugs")
           "* TODO %?\n:Description:\n%^T\n%i %a\n" :prepend t)
-          ("b" "breakthroughs" entry (file+headline "~/Documents/org_files/current_bugs.org" "Breakthroughs!")
+          ("b" "breakthroughs" entry (file+headline "~/org/current_bugs.org" "Breakthroughs!")
           "* DONE %?\n:Description:\n%^T\n%i %a\n"))))
 
 ;; where we set the org-agenda files to look for TODO values..
-(setq org-agenda-files (list "~/Documents/org_files/work_done.org"
-                             "~/Documents/org_files/current_bugs.org"
+(setq org-agenda-files (list "~/org/work_done.org"
+                             "~/org/current_bugs.org"
                              ;; added by the lord
-                             "~/Documents/org_files/org_notes.org"
-                             "~/Documents/org_files/todo.org"
-                             "~/Documents/org_files/journal.org"
+                             "~/org/org_notes.org"
+                             "~/org/todo.org"
+                             "~/org/journal.org"
                              ;; not currently using gcal..
-                             "~/Documents/org_files/gcal.org"))
+                             "~/org/gcal.org"))
 
 ;; straght from the lord hisself..
 ;; https://github.com/hlissner/doom-emacs/tree/develop/modules/lang/cc#macos
 ;; solution below fixes unncessary irony problems with MacOS stuff, solution i found at URL below..
 ;; https://github.com/Sarcasm/irony-mode/issues/507
 ;; also useful command from: https://github.com/Microsoft/vscode-cpptools/issues/1268 - clang -v -E -x c++ - < /dev/null
-(setq irony-additional-clang-options
-      (append '("-I" "/usr/local/Cellar/llvm/10.0.0_3/include/c++/v1") irony-additional-clang-options))
-(setq irony-additional-clang-options
-      (append '("-I" "/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/include") irony-additional-clang-options))
-;; needed this 2/12/2020..
-(setq irony-additional-clang-options
-      (append '("-I" "/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib/clang/11.0.3/include") irony-additional-clang-options))
-(setq irony-additional-clang-options
-      (append '("-std=c++11") irony-additional-clang-options))
-;; using homebrew to manage root now.. fixing tf1 import errors and switching to manage python with pyenv 07/06/2020
-(setq irony-additional-clang-options
-      (append '("-I" "/usr/local/Cellar/root/6.22.00_1/include/root") irony-additional-clang-options))
+;; (setq irony-additional-clang-options
+;;       (append '("-I" "/usr/local/Cellar/llvm/10.0.0_3/include/c++/v1") irony-additional-clang-options))
+;; (setq irony-additional-clang-options
+;;       (append '("-I" "/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/include") irony-additional-clang-options))
+;; ;; needed this 2/12/2020..
+;; (setq irony-additional-clang-options
+;;       (append '("-I" "/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib/clang/11.0.3/include") irony-additional-clang-options))
+;; (setq irony-additional-clang-options
+;;       (append '("-std=c++11") irony-additional-clang-options))
+;; ;; using homebrew to manage root now.. fixing tf1 import errors and switching to manage python with pyenv 07/06/2020
+;; (setq irony-additional-clang-options
+;;       (append '("-I" "/usr/local/Cellar/root/6.22.00_1/include/root") irony-additional-clang-options))
 
 ;; ;; check to see if python finally figured it out with this one..
 ;; (setq python-shell-interpreter "python3"
@@ -165,7 +163,7 @@
 (setq lsp-pyls-plugins-pycodestyle-max-line-length 88)
 
 ;; we don't want this stupid checker
-(setq flycheck-disabled-checkers '(python-pylint))
+;; (setq flycheck-disabled-checkers '(python-pylint))
 
 (setq +python-ipython-repl-args '("-i" "--simple-prompt" "--no-color-info"))
 (setq +python-jupyter-repl-args '("--simple-prompt"))
@@ -175,7 +173,7 @@
 ;; things for google calendar syncing..
 ;; (setq org-gcal-client-id "961006309840-31rc93jb94bqvf0oj3s0bb4qbq86aes0.apps.googleusercontent.com"
 ;;       org-gcal-client-secret "qC8jc8BBJEPFtTs8mvJEm9RX"
-;;       org-gcal-file-alist '(("keefekevin91@gmail.com" .  "~/Documents/org_files/gcal.org")))
+;;       org-gcal-file-alist '(("keefekevin91@gmail.com" .  "~/org/gcal.org")))
 
 ;;; projectile things
 (setq projectile-sort-order 'recentf) ;; want to open most recent things
@@ -183,8 +181,8 @@
 ;; (setq projectile-globally-ignored-directories  '("/Users/kevinkeefe/Documents/SVSC-Work-Stations/WorkStation3/svsc_irs/build"))
 
 ;;  tramp things
-(setq tramp-default-user "kevinpk") ;; useful for mtc connection
-(setq tramp-default-remote-shell "/usr/bin/bash")
+;; (setq tramp-default-user "kevinpk") ;; useful for mtc connection
+;; (setq tramp-default-remote-shell "/usr/bin/bash")
 
 ;; c++ projects:
 ;; we want the svsc_irs project separate:
@@ -193,7 +191,7 @@
 ;; python projects:
 
 ;; todoist
-(setq todoist-token "351f50b1999cb691178bed76dea651f838079d47")
+;; (setq todoist-token "351f50b1999cb691178bed76dea651f838079d47")
 
 ;;; vhdl-tool
 ;; (flycheck-define-checker vhdl-tool
@@ -224,3 +222,5 @@
 ;;     (smtpmail-smtp-user     . "keefekevin91@gmail.com")
 ;;     (mu4e-compose-signature . "---\nKevin Keefe"))
 ;;   t)
+
+;;;

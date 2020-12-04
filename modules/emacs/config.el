55e87973017e11e98cff4e9a5aee2f4957123aee
kkeefe <kevinpk@hawaii.edu>
2020-12-04 14:01:16 -0800

    personal mac commits, semi merging config and init files between desktop and personal mac

--- modules/emacs/config.el
+++ modules/emacs/config.el
@@ -6,7 +6,7 @@
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets.
 (setq user-full-name "Kevin Keefe"
-      user-mail-address "keefekevin91@gmail.com")
+      user-mail-address "kevinpk@hawaii.edu")
 
 ;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
 ;; are the three important ones:
@@ -29,6 +29,8 @@
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
 (setq org-directory "~/org/")
+;; local setqs
+(setq default-directory "~/")
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
@@ -79,9 +81,6 @@
 (after! projectile
   (add-to-list 'projectile-globally-ignored-directories '"build"))
 
-;; local setqs
-(setq default-directory "~/")
-
 ;; for configuring more org things we want
 (after! org
   (setq org-agenda-files
@@ -89,3 +88,139 @@
   (advice-add 'org-refile :after 'org-save-all-org-buffers))
 ;; taken from org website, append notes to the org file?
 (setq org-default-notes-file (concat org-directory "/notes.org"))
+
+;; ;; set this because i'm tired of accidentally reverting..
+(map! :leader
+      (:prefix "q"
+        :n "l" #'evil-window-right
+        :n "h" #'evil-window-left))
+
+;; Switch to the new window after splitting
+(setq evil-split-window-below t
+      evil-vsplit-window-right t)
+
+;; just support this stuff bru
+(setq org-list-allow-alphabetical t)
+
+;; additional personal things:
+;; ;; org keybinds to set
+(global-set-key (kbd "C-c a") 'org-agenda)
+(global-set-key (kbd "C-c c") 'org-capture)
+(global-set-key (kbd "C-c f") 'org-footnote-action)
+
+;; list of custom org templates
+(after! org
+  (setq org-capture-templates
+        '(("w" "work" entry (file+headline "~/Documents/org_files/work_done.org" "Work_Tasks")
+          "* TODO %?\n:Description:\n %^t \n %i \n")
+          ("s" "self-stuff" entry (file+headline "~/Documents/org_files/work_done.org" "Self_Tasks")
+          "* TODO %?\n:Description:\n %^t \n %i \n")
+          ("v" "quick work" entry (file+headline "~/Documents/org_files/work_done.org" "Quick_Tasks")
+          "* TODO %?\n:Description:\n %^t \n %i \n")
+          ("c" "creative" entry (file+headline "~/Documents/org_files/work_done.org" "Creative_Work")
+          "* TODO %?\n:Description:\n%^t \n %i \n")
+          ("p" "code problems" entry (file+headline "~/Documents/org_files/current_bugs.org" "Current Bugs")
+          "* TODO %?\n:Description:\n%^T\n%i %a\n" :prepend t)
+          ("b" "breakthroughs" entry (file+headline "~/Documents/org_files/current_bugs.org" "Breakthroughs!")
+          "* DONE %?\n:Description:\n%^T\n%i %a\n"))))
+
+;; where we set the org-agenda files to look for TODO values..
+(setq org-agenda-files (list "~/Documents/org_files/work_done.org"
+                             "~/Documents/org_files/current_bugs.org"
+                             ;; added by the lord
+                             "~/Documents/org_files/org_notes.org"
+                             "~/Documents/org_files/todo.org"
+                             "~/Documents/org_files/journal.org"
+                             ;; not currently using gcal..
+                             "~/Documents/org_files/gcal.org"))
+
+;; straght from the lord hisself..
+;; https://github.com/hlissner/doom-emacs/tree/develop/modules/lang/cc#macos
+;; solution below fixes unncessary irony problems with MacOS stuff, solution i found at URL below..
+;; https://github.com/Sarcasm/irony-mode/issues/507
+;; also useful command from: https://github.com/Microsoft/vscode-cpptools/issues/1268 - clang -v -E -x c++ - < /dev/null
+(setq irony-additional-clang-options
+      (append '("-I" "/usr/local/Cellar/llvm/10.0.0_3/include/c++/v1") irony-additional-clang-options))
+(setq irony-additional-clang-options
+      (append '("-I" "/Library/Developer/CommandLineTools/SDKs/MacOSX.sdk/usr/include") irony-additional-clang-options))
+;; needed this 2/12/2020..
+(setq irony-additional-clang-options
+      (append '("-I" "/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib/clang/11.0.3/include") irony-additional-clang-options))
+(setq irony-additional-clang-options
+      (append '("-std=c++11") irony-additional-clang-options))
+;; using homebrew to manage root now.. fixing tf1 import errors and switching to manage python with pyenv 07/06/2020
+(setq irony-additional-clang-options
+      (append '("-I" "/usr/local/Cellar/root/6.22.00_1/include/root") irony-additional-clang-options))
+
+;; ;; check to see if python finally figured it out with this one..
+;; (setq python-shell-interpreter "python3"
+;;       flycheck-python-pycompile-executable "python3")
+
+;; including based on recommendation from link:
+;; https://github.com/hlissner/doom-emacs/tree/develop/modules/lang/python
+(after! lsp-python-ms
+  (set-lsp-priority! 'mspyls 1))
+
+;; we want to ignore anything longer than 88
+(setq lsp-pyls-plugins-pycodestyle-max-line-length 88)
+
+;; we don't want this stupid checker
+(setq flycheck-disabled-checkers '(python-pylint))
+
+(setq +python-ipython-repl-args '("-i" "--simple-prompt" "--no-color-info"))
+(setq +python-jupyter-repl-args '("--simple-prompt"))
+
+;;; basic package configurations beyond language:
+
+;; things for google calendar syncing..
+;; (setq org-gcal-client-id "961006309840-31rc93jb94bqvf0oj3s0bb4qbq86aes0.apps.googleusercontent.com"
+;;       org-gcal-client-secret "qC8jc8BBJEPFtTs8mvJEm9RX"
+;;       org-gcal-file-alist '(("keefekevin91@gmail.com" .  "~/Documents/org_files/gcal.org")))
+
+;;; projectile things
+(setq projectile-sort-order 'recentf) ;; want to open most recent things
+(setq projectile-auto-discover 'nil) ;; don't get projects we don't want..
+;; (setq projectile-globally-ignored-directories  '("/Users/kevinkeefe/Documents/SVSC-Work-Stations/WorkStation3/svsc_irs/build"))
+
+;;  tramp things
+(setq tramp-default-user "kevinpk") ;; useful for mtc connection
+(setq tramp-default-remote-shell "/usr/bin/bash")
+
+;; c++ projects:
+;; we want the svsc_irs project separate:
+;; (projectile-register-project-type 'svsc_irs ')
+
+;; python projects:
+
+;; todoist
+(setq todoist-token "351f50b1999cb691178bed76dea651f838079d47")
+
+;;; vhdl-tool
+;; (flycheck-define-checker vhdl-tool
+;;   "A VHDL syntax checker, type checker and linter using VHDL-Tool.
+
+;; See URL `http://vhdltool.com'."
+;;   :command ("vhdl-tool" "client" "lint" "--compact" "--stdin" "-f" source
+;;             )
+;;   :standard-input t
+;;   :error-patterns
+;;   ((warning line-start (file-name) ":" line ":" column ":w:" (message) line-end)
+;;    (error line-start (file-name) ":" line ":" column ":e:" (message) line-end))
+;;   :modes (vhdl-mode))
+
+;; (add-to-list 'flycheck-checkers 'vhdl-tool)
+
+;; ;; then include the lsp path
+;; (after! lsp-vhdl-server
+;;   (setq lsp-vhdl-server-path "vhdl-tool"))
+
+;; ;; mu4e things
+;; ;; Each path is relative to `+mu4e-mu4e-mail-path', which is ~/.mail by default
+;; (set-email-account! "keefekevin91@gmail.com"
+;;   '((mu4e-sent-folder       . "/keefekevin91@gmail.com/Sent Mail")
+;;     (mu4e-drafts-folder     . "/keefekevin91@gmail.com/Drafts")
+;;     (mu4e-trash-folder      . "/keefekevin91@gmail.com/Trash")
+;;     (mu4e-refile-folder     . "/keefekevin91@gmail.com/All Mail")
+;;     (smtpmail-smtp-user     . "keefekevin91@gmail.com")
+;;     (mu4e-compose-signature . "---\nKevin Keefe"))
+;;   t)
--- modules/emacs/init.el
+++ modules/emacs/init.el
@@ -66,7 +66,7 @@
        ;;word-wrap         ; soft wrapping with language-aware indent
 
        :emacs
-       dired             ; making dired pretty [functional]
+       (dired +icons)             ; making dired pretty [functional]
        electric          ; smarter, keyword-based electric-indent
        ;;ibuffer         ; interactive buffer management
        undo              ; persistent, smarter undo for your inevitable mistakes
@@ -102,7 +102,7 @@
        ;;rgb               ; creating color strings
        ;;taskrunner        ; taskrunner for all your projects
        ;;terraform         ; infrastructure as code
-       tmux              ; an API for interacting with tmux
+       ;;tmux              ; an API for interacting with tmux
        ;;upload            ; map local to remote projects via ssh/ftp
 
        :os

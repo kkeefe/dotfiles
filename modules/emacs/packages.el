58e020d583de9558f2b8f6c0a4e28d450248e96d
kkeefe <kevinpk@hawaii.edu>
2020-12-04 14:03:11 -0800

    more packages to take care after

--- modules/emacs/packages.el
+++ modules/emacs/packages.el
@@ -5,11 +5,17 @@
 ;; on the command line, then restart Emacs for the changes to take effect -- or
 ;; use 'M-x doom/reload'.
 
-
 ;; To install SOME-PACKAGE from MELPA, ELPA or emacsmirror:
 ;(package! some-package)
 
+;; debugging
 (package! dap-mode)
+
+;; todoist
+(package! todoist)
+
+;; vhdl tool because we want syntax highlighting at least for out vhdl stuff
+(package! vhdl-tools)
 
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
